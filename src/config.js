dd05a6504c1abce0ba6293c52615d0cf58206436
cicerchiadaniele <cicerchiadaniele@gmail.com>
2026-09-17 14:27:07 +0200

    Delete src directory

--- src/config.js
+++
@@ -1,5 +0,0 @@
-// Webhook Make: scenario "Studio CAI – WebApp Registro Chiavi" (ID 7434486)
-export const WEBHOOK_URL = 'https://hook.eu1.make.com/bxtcwfhdeqixkixygyh8m4u59xfmlcxo'
-export const APP_VERSION = '1.0.0'
-export const GIORNI_AVVISO = 5
-export const TIMEOUT_MS = 25000
